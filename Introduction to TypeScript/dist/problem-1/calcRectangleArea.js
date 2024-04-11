0345763236586b2800997d116a11898327011e10
Ioan Velev <ioanvelev46@gmail.com>
2024-04-11 21:47:31 +0300

    Solve problem 1

---
+++ Introduction to TypeScript/dist/problem-1/calcRectangleArea.js
@@ -0,0 +1,8 @@
+// Function declaration
+function calcRectangleArea(a, b) {
+    return a * b;
+}
+// Arrow function
+const calcArea = (a, b) => a * b;
+console.log(calcRectangleArea(5, 7));
+console.log(calcArea(6, 8));
---
+++ Introduction to TypeScript/src/problem-1/calcRectangleArea.ts
@@ -0,0 +1,13 @@
+// Function declaration
+
+function calcRectangleArea(a: number, b: number): number {
+    return a * b
+}
+
+
+// Arrow function
+const calcArea = (a: number, b: number): number => a * b;
+
+
+console.log(calcRectangleArea(5, 7));
+console.log(calcArea(6, 8));
