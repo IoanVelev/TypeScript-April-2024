489377f7f41d6ddafa559da1a09220d6dff2b138
Ioan Velev <ioanvelev46@gmail.com>
2024-04-23 23:50:57 +0300

    Solve task 2

---
+++ Generics/dist/task_2/genericElements.js
@@ -0,0 +1,14 @@
+class CompareElement {
+    array;
+    constructor(input1) {
+        this.array = input1;
+    }
+    compare(input2) {
+        const res = this.array.filter(el => el === input2);
+        return res.length;
+    }
+}
+const elements = new CompareElement(['a', 'b', 'ab', 'abc', 'cba', 'b']);
+console.log(elements.compare('b'));
+let c = new CompareElement([1, 2, 3, 4, 5, 1, 1]);
+console.log(c.compare(1));
---
+++ Generics/src/task_2/genericElements.ts
@@ -0,0 +1,23 @@
+class CompareElement<T> {
+array: T[];
+
+constructor(input1: T[]) {
+    this.array = input1;
+}
+
+compare(input2: T): number {
+const res = this.array.filter(el => el === input2);
+
+return res.length;
+}
+}
+
+const elements = new CompareElement(['a', 'b', 'ab', 'abc', 'cba', 'b']);
+
+
+console.log(elements.compare('b'));
+
+let c = new CompareElement([1, 2, 3, 4, 5, 1, 1]);
+
+console.log(c.compare(1));
+
