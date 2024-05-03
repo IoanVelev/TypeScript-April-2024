15def50853e7c790973e7d6f344c94a5b708adea
Ioan Velev <ioanvelev46@gmail.com>
2024-05-03 19:03:02 +0300

    Update index.ts

--- Namespaces and Modules/namespaces-demo/dist/index.js
+++ Namespaces and Modules/namespaces-demo/dist/index.js
@@ -1,19 +1,28 @@
-var FootballGroup;
-(function (FootballGroup) {
-    class FootballTeam {
-        constructor(teamName, country, totalPlayers) {
-            this.teamName = teamName;
-            this.country = country;
-            this.totalPlayers = totalPlayers;
-            this.teamName = teamName;
-            this.country = country;
-            this.totalPlayers = totalPlayers;
-        }
-        getTeamDetails() {
-            return `Football club ${this.teamName} is from ${this.country} and the squad consists of ${this.totalPlayers} players`;
-        }
-    }
-    FootballGroup.FootballTeam = FootballTeam;
-})(FootballGroup || (FootballGroup = {}));
-const club1 = new FootballGroup.FootballTeam('Manchester United', 'England', 29);
-console.log(club1.getTeamDetails());
+"use strict";
+// namespace FootballGroup {
+//     interface FootballTeamType {
+//         teamName: string;
+//         country: string;
+//         totalPlayers: number
+//     }
+Object.defineProperty(exports, "__esModule", { value: true });
+//     export class FootballTeam implements FootballTeamType {
+//         teamName: string;
+//         country: string;
+//         totalPlayers: number;
+//         constructor(teamName: string, country: string, totalPlayers: number) {
+//             this.teamName = teamName;
+//             this.country = country;
+//             this.totalPlayers = totalPlayers;
+//         }
+//         getTeamDetails(): string{
+//             return `Football club ${this.teamName} is from ${this.country} and the squad consists of ${this.totalPlayers} players`;
+//         }
+//     }
+// }
+// const club1 = new FootballGroup.FootballTeam('Manchester United', 'England', 29);
+// console.log(club1.getTeamDetails());
+/// <reference path="tvShows.ts"/>
+const tvShows_1 = require("./tvShows");
+const tvShow1 = new tvShows_1.TvShowsGroup.TvShows('The Bing Bang Theory', ['Sheldon Cooper', 'Leonard Hipersfald', 'Howard Wolowitz', 'Penny']);
+console.log(tvShow1.getTvShowDetails());
--- Namespaces and Modules/namespaces-demo/src/index.ts
+++ Namespaces and Modules/namespaces-demo/src/index.ts
@@ -1,28 +1,32 @@
-namespace FootballGroup {
-    interface FootballTeamType {
-        teamName: string;
-        country: string;
-        totalPlayers: number
-    }
+// namespace FootballGroup {
+//     interface FootballTeamType {
+//         teamName: string;
+//         country: string;
+//         totalPlayers: number
+//     }
 
-    export class FootballTeam implements FootballTeamType {
-        teamName: string;
-        country: string;
-        totalPlayers: number;
+//     export class FootballTeam implements FootballTeamType {
+//         teamName: string;
+//         country: string;
+//         totalPlayers: number;
 
-        constructor(teamName: string, country: string, totalPlayers: number) {
-            this.teamName = teamName;
-            this.country = country;
-            this.totalPlayers = totalPlayers;
-        }
-
-
-        getTeamDetails(): string{
-            return `Football club ${this.teamName} is from ${this.country} and the squad consists of ${this.totalPlayers} players`;
-        }
-    }
-}
+//         constructor(teamName: string, country: string, totalPlayers: number) {
+//             this.teamName = teamName;
+//             this.country = country;
+//             this.totalPlayers = totalPlayers;
+//         }
 
 
-const club1 = new FootballGroup.FootballTeam('Manchester United', 'England', 29);
-console.log(club1.getTeamDetails());
+//         getTeamDetails(): string{
+//             return `Football club ${this.teamName} is from ${this.country} and the squad consists of ${this.totalPlayers} players`;
+//         }
+//     }
+// }
+
+
+// const club1 = new FootballGroup.FootballTeam('Manchester United', 'England', 29);
+// console.log(club1.getTeamDetails());
+/// <reference path="tvShows.ts"/>
+import { TvShowsGroup } from './tvShows';
+const tvShow1 = new TvShowsGroup.TvShows('The Bing Bang Theory', ['Sheldon Cooper', 'Leonard Hipersfald', 'Howard Wolowitz', 'Penny']);
+console.log(tvShow1.getTvShowDetails());
